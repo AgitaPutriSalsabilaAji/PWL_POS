8b22968a3ba77b6433af0dd9ed6c9306e912f1e7
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2025-03-02 16:36:28 +0700

    QUERY BUILDER

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use Database\Seeders\KategoriSeeder;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +10,5 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
