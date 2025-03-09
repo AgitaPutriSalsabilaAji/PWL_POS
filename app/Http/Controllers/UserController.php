cf30a0b1a1ec42ec331953cd2932920a793f8885
Agita Putri Salsabila Aji <agitaputri513@gmail.com>
2025-03-09 20:45:19 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,24 +12,15 @@ class UserController extends Controller
     public function index()
     {
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
+            'level_id' => 2,
+            'username' => 'manajer_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::insert($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]); 
 
-        
-        /*$data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-
-        UserModel::where('username', 'customer-1')->update($data);
-
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);*? */
     }
 }
